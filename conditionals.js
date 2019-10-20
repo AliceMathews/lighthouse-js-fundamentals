8ec1962f177a870c7a2cf60263fa93c1c948e7b6
AliceMathews <alice_mathews@hotmail.co.uk>
2019-10-20 02:26:43 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,5 +1,35 @@
-const raining = false; 
-const cold = false; 
+const isCitizen = true; 
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote");
+}
+
+const temperature = -45;
+
+if(temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea");
+}
+
+const raining = true; 
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//const raining = false; 
+//const cold = false; 
 
 /*if (raining) {
   console.log("Don't forget your umbrella!");
@@ -17,7 +47,7 @@ console.log("Now you're ready to go outside!");*/
   console.log("short sleeves are fine");
 }*/
 
-const temperature = -1; 
+/*const temperature = -1; 
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -27,4 +57,4 @@ if (temperature < 0) {
   console.log("Short sleeves are fine");
 }
 
-console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!");*/
